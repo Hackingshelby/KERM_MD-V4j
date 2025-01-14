5266f82eea15c5c39a5859721f3d69cd2c8f283f
Hackingshelby <hackingthomas00@gmail.com>
2025-01-14 16:30:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aBYDFRTY#XsFdDMpHEm2qoetc6IH0HZkW9TaCruabqfTRMOyAJmQ",
+SESSION_ID: process.env.SESSION_ID || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/085c4b1068f0f4f8db970.mp4",
-ALIVE_MSG: process.env.ALIVE_MSG || "𝖧𝖨𝖨 𝖣𝖤𝖠𝖱 𝖨𝖬 𝖮𝖭𝖫𝖨𝖭𝖤 𝖨，𝖬 『NARUTO-MD』 𝖶𝖧𝖠𝖳𝖲𝖠𝖯𝖯 𝖡𝖮𝖳.",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/m87k0f.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "𝖧𝖨𝖨 𝖣𝖤𝖠𝖱 𝖨𝖬 𝖮𝖭𝖫𝖨𝖭𝖤 𝖨，𝖬 『KERM_MD-V4 』 𝖶𝖧𝖠𝖳𝖲𝖠𝖯𝖯 𝖡𝖮𝖳.",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
@@ -20,6 +20,6 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "➺『NARUTO-MD』",
+BOT_NAME: process.env.BOT_NAME || "➺『KERM_MD-V4 』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
